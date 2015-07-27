b8f32acee1dbcb6746f8dfc4444bc328bcd55d0b
emanuelbatista <emanuelbatista2011@gmail.com>
2015-07-27 19:33:47 -0300

    refatorando o código

--- blogsoon-webapp/src/main/java/br/edu/ifpb/blogsoon/webapp/controller/MainController.java
+++ blogsoon-webapp/src/main/java/br/edu/ifpb/blogsoon/webapp/controller/MainController.java
@@ -2,9 +2,8 @@ package br.edu.ifpb.blogsoon.webapp.controller;
 
 import br.edu.ifpb.blogsoon.core.entidades.Usuario;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  *
@@ -12,13 +11,15 @@ import org.springframework.web.bind.annotation.SessionAttributes;
  * @version 0.1
  */
 @Controller
-@RequestMapping("/")
-@SessionAttributes("usuario")
 public class MainController {
 
+    @ModelAttribute("usuario")
+    public Usuario criarUsuario() {
+        return new Usuario();
+    }
+    
     @RequestMapping("/")
-    public String index (Model model){
-        model.addAttribute("usuario", new Usuario());
+    public String index (){
         return "index";
     }
 }
